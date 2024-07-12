69b6bc4ca2f0f9822fd42e9cb247df7d7fb14f99
wavyStructures <anja_schwab@gmx.de>
2024-07-12 21:53:41 +0200

    working on fullscreen

--- js/game.js
+++ js/game.js
@@ -4,10 +4,19 @@ let world;
 let sounds = new Sounds();
 let audioMute = true;
 
-function startPage() {
+// function startPage() {
 
+//     canvas = document.getElementById("canvas");
+//     canvas.style.backgroundImage = 'url("/img/9_intro_outro_screens/start/startscreen_1.png")';
+// }
+
+function startPage() {
     canvas = document.getElementById("canvas");
     canvas.style.backgroundImage = 'url("/img/9_intro_outro_screens/start/startscreen_1.png")';
+    canvas.style.backgroundRepeat = 'no-repeat';
+    canvas.style.backgroundPosition = 'center';
+    adaptCanvasBackground();
+    canvas.onclick = startGame;
 }
 
 function init() {
@@ -88,9 +97,18 @@ function adaptInnerWidth() {
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
     } else {
-        canvas.backgroundSize = 720, 480;
+        // canvas.backgroundSize = 720, 480;
+        canvas.width = 720;
+        canvas.height = 480;
     }
+    adaptCanvasBackground();
 }
+
+function adaptCanvasBackground() {
+    canvas.style.backgroundSize = `${canvas.width}px ${canvas.height}px`;
+}
+
+
 
 function toggleAudio() {
     let audioIcon = document.getElementById('audioIcon');
@@ -102,8 +120,56 @@ function toggleAudio() {
     audioMute = !audioMute;
 }
 
+// function toggleFullScreen() {
+
+//     if (!document.fullscreenElement) {
+//         let fullScreenDiv = document.getElementById('fullScreenDiv');
+//         fullScreenDiv.requestFullscreen();
+//     } else {
+//         fullScreenDiv.exitFullscreen();
+//     }
+// }
 
 
+function fullscreen() {
+    let fullscreenElement = document.fullscreenElement || document.webkitFullscreenElement || document.mozFullScreenElement || document.msFullscreenElement;
+    if (fullscreenElement) {
+        exitFullscreen();
+    } else {
+        let fullscreen = document.getElementById('fullScreenDiv');
+        enterFullscreen(fullscreen);
+    }
+
+}
+
+function enterFullscreen(element) {
+    document.getElementById('canvas').style.width = '-webkit-fill-available';
+    document.getElementById('canvas').style.height = '-webkit-fill-available';
+    // document.getElementById('fullScrenButton').style.display = 'none';
+    // document.getElementById('exitFullScreenButton').style.display = 'block';
+    if (element.requestFullscreen) {
+        element.requestFullscreen();
+    } else if (element.msRequestFullscreen) {      // for IE11 (remove June 15, 2022)
+        element.msRequestFullscreen();
+    } else if (element.webkitRequestFullscreen) {  // iOS Safari
+        element.webkitRequestFullscreen();
+    }
+}
+
+function closeFullscreen() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) { /* Safari */
+        document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { /* IE11 */
+        document.msExitFullscreen();
+    }
+}
+
+//     }
+
+//     handleFullscreenChange() {
+//         this.world.fullscreenOn = !document.fullscreenElement;
 
 
 // function closeFullscreen() {
@@ -128,6 +194,7 @@ function removeWinOverlay() {
 
 
 function startGame() {
+    // canvas.onclick = startGame;
     removeWinOverlay();
     sounds.playSound(sounds.come_on_sound);
     init();

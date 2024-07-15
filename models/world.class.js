add2142c3ec0102a8a78b14c3e63eaa027fb5304
wavyStructures <anja_schwab@gmx.de>
2024-07-15 11:27:04 +0200

    draw character after statusbars

--- models/world.class.js
+++ models/world.class.js
@@ -177,7 +177,7 @@ class World {
         this.ctx.translate(this.camera_x, 0);
 
         this.addObjectsToMap(this.level.backgroundObjects);
-        this.addToMap(this.character);
+
         this.addObjectsToMap(this.level.clouds);
 
         this.ctx.translate(-this.camera_x, 0);
@@ -191,7 +191,7 @@ class World {
         // ------------  space for more moving objects ----------------
         this.ctx.translate(this.camera_x, 0);
 
-
+        this.addToMap(this.character);
         this.addObjectsToMap(this.level.enemies);
 
         this.addObjectsToMap(this.throwableObjects);

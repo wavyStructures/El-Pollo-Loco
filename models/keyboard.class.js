e0aad92fc3ef41ad1bc4ac1a7915e1aedd46d97a
wavyStructures <anja_schwab@gmx.de>
2024-07-30 15:13:00 +0200

    }, { passive: false }); on EventListener bindBtnPressEvents()

--- models/keyboard.class.js
+++ models/keyboard.class.js
@@ -87,42 +87,42 @@ class Keyboard {
         document.getElementById('btnLeft').addEventListener('touchstart', (e) => {
             e.preventDefault();
             this.LEFT = true;
-        });
+        }, { passive: false });
         document.getElementById('btnLeft').addEventListener('touchend', (e) => {
             e.preventDefault();
             this.LEFT = false;
-        });
+        }, { passive: false });
         document.getElementById('btnRight').addEventListener('touchstart', (e) => {
             e.preventDefault();
             this.RIGHT = true;
-        });
+        }, { passive: false });
         document.getElementById('btnRight').addEventListener('touchend', (e) => {
             e.preventDefault();
             this.RIGHT = false;
-        });
+        }, { passive: false });
         document.getElementById('btnJump').addEventListener('touchstart', (e) => {
             e.preventDefault();
             this.SPACE = true;
-        });
+        }, { passive: false });
         document.getElementById('btnJump').addEventListener('touchend', (e) => {
             e.preventDefault();
             this.SPACE = false;
-        });
+        }, { passive: false });
         document.getElementById('btnThrow').addEventListener('touchstart', (e) => {
             e.preventDefault();
             this.D = true;
-        });
+        }, { passive: false });
         document.getElementById('btnThrow').addEventListener('touchend', (e) => {
             e.preventDefault();
             this.D = false;
-        });
+        }, { passive: false });
         document.getElementById('btnBuy').addEventListener('touchstart', (e) => {
             e.preventDefault();
             this.B = true;
-        });
+        }, { passive: false });
         document.getElementById('btnBuy').addEventListener('touchend', (e) => {
             e.preventDefault();
             this.B = false;
-        });
+        }, { passive: false });
     }
 }
